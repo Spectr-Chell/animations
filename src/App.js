a8c982124eb5c32582fb91b0e2fcda83a39f0e64
Spectr-Chell <145916894+Spectr-Chell@users.noreply.github.com>
2024-12-11 21:27:09 +0300

    ю

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import ChoiseSection2 from "./component/Section/ChoiseSection2";
 import ChoiseSection3 from "./component/Section/ChoiseSection3";
 import ChoiseSection4 from "./component/Section/ChoiseSection4";
 import ChoiseSection5 from "./component/Section/ChoiseSection5";
+import ChoiseSection6 from "./component/Section/ChoiseSection6";
+
+const sections = [
+  ChoiseSection1,
+  ChoiseSection2,
+  ChoiseSection3,
+  ChoiseSection4,
+  ChoiseSection5,
+  ChoiseSection6,
+];
 
 export default function App() {
-
   return (
     <div className="App">
-      <ChoiseSection1></ChoiseSection1>
-      <ChoiseSection2></ChoiseSection2>
-      <ChoiseSection3></ChoiseSection3>
-      <ChoiseSection4></ChoiseSection4>
-      <ChoiseSection5></ChoiseSection5>
+      {sections.map((Section, index) => (
+        <Section key={index} />
+      ))}
     </div>
   );
 }
